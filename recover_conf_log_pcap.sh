0d30dfccf501bca45b57cb5eaab6b12eea265b62
saadsakib <saadsakib3@gmail.com>
2021-03-16 09:43:46 +0600

    Fetch home mme conf log + both epc attach success

--- recover_conf_log_pcap.sh
+++ recover_conf_log_pcap.sh
@@ -4,12 +4,14 @@ time_dir="`date +%H_%M`"
 archive_dir=./archives
 
 log_dir=$archive_dir/$date_dir/$time_dir
-mkdir -p $log_dir/oai-hss-cfg $log_dir/oai-mme-cfg $log_dir/oai-spgwc-cfg $log_dir/oai-spgwu-cfg
+mkdir -p $log_dir
+# mkdir -p $log_dir/oai-hss-cfg $log_dir/oai-mme-cfg $log_dir/oai-spgwc-cfg $log_dir/oai-spgwu-cfg
 
 # retrieve the modified configuration files
 docker cp prod-oai-hss:/openair-hss/etc/. $log_dir/oai-hss-cfg
 docker cp prod-oai-hss-home:/openair-hss/etc/. $log_dir/oai-home-hss-cfg
 docker cp prod-oai-mme:/openair-mme/etc/. $log_dir/oai-mme-cfg
+docker cp prod-oai-mme-home:/openair-mme/etc/. $log_dir/oai-home-mme-cfg
 docker cp prod-oai-spgwc:/openair-spgwc/etc/. $log_dir/oai-spgwc-cfg
 docker cp prod-oai-spgwu-tiny:/openair-spgwu-tiny/etc/. $log_dir/oai-spgwu-cfg
 
@@ -17,6 +19,7 @@ docker cp prod-oai-spgwu-tiny:/openair-spgwu-tiny/etc/. $log_dir/oai-spgwu-cfg
 docker cp prod-oai-hss:/openair-hss/hss_check_run.log $log_dir
 docker cp prod-oai-hss-home:/openair-hss/home_hss_check_run.log $log_dir
 docker cp prod-oai-mme:/openair-mme/mme_check_run.log $log_dir
+docker cp prod-oai-mme-home:/openair-mme/home_mme_check_run.log $log_dir
 docker cp prod-oai-spgwc:/openair-spgwc/spgwc_check_run.log $log_dir
 docker cp prod-oai-spgwu-tiny:/openair-spgwu-tiny/spgwu_check_run.log $log_dir
 
@@ -24,5 +27,6 @@ docker cp prod-oai-spgwu-tiny:/openair-spgwu-tiny/spgwu_check_run.log $log_dir
 docker cp prod-oai-hss:/tmp/hss_check_run.pcap $log_dir
 docker cp prod-oai-hss-home:/tmp/home_hss_check_run.pcap $log_dir
 docker cp prod-oai-mme:/tmp/mme_check_run.pcap $log_dir
+docker cp prod-oai-mme-home:/tmp/home_mme_check_run.pcap $log_dir
 docker cp prod-oai-spgwc:/tmp/spgwc_check_run.pcap $log_dir
 docker cp prod-oai-spgwu-tiny:/tmp/spgwu_check_run.pcap $log_dir
